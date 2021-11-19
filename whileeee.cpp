40e7e156a5c1bb1d1b154394a6c8dbef43c12251
Ikhsan saputra <92314859+Ikhssptr25@users.noreply.github.com>
2021-11-19 20:53:53 +0700

    Add files via upload

---
+++ whileeee.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    /*ikhsan saputra
+	2117051037
+	kelas B*/
+	  
+	//deklarasi variabel//  
+    int i;
+	int k;
+	
+	
+    //peruangan for
+    for(i = 0; i<5; i++){
+    	for(k=0; k<i+1; k++){
+		cout<<"*";
+		}
+    cout<<endl;
+	}
+	
+	 for(i = 6; i>0; i--){
+    	for(k=0; k<=i-2; k++){
+		cout<<"*";
+		}
+    cout<<endl;
+	}
+
+    
+    
+    return 0;
+	
+	
+
+}
