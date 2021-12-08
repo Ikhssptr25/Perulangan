09bdf0b5c3ce99596f00e825585e653e525b8301
Ikhsan saputra <92314859+Ikhssptr25@users.noreply.github.com>
2021-12-08 12:41:32 +0700

    Add files via upload

---
+++ polabintang.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+string p[10]={"*"," *"," *"," *"," *"," *"," *"," *"," *"," *"};
+for (int i = 0; i < 10; i++){
+for(int j = 0; j < 10; j++){
+cout << p[i];
+}
+cout << endl;
+}
+return 0;
+}
